a326195735d539a4be39a8ea2ee8ea1a8cf58c39
Max Menarini <mmenarini@ucsd.edu>
2013-10-23 04:26:35 -0700

    Lab 4

---
+++ ChatClient/src/main/java/edu/ucsd/cse110/client/Constants.java
@@ -0,0 +1,8 @@
+package edu.ucsd.cse110.client;
+
+public interface Constants {
+	public static String ACTIVEMQ_URL = "tcp://localhost:61616";
+	public static String USERNAME = "max";	
+	public static String PASSWORD = "pwd";	
+	public static String QUEUENAME = "test";
+}
---
+++ ChatServer/src/main/java/edu/ucsd/cse110/server/Server.java
@@ -0,0 +1,8 @@
+package edu.ucsd.cse110.server;
+
+public class Server {
+
+	public void receive(String msg) {
+		System.out.println(msg);		
+	}
+}
