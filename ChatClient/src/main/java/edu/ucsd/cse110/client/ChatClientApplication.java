b5f6f6498dc579889d0b160f30d49ac3c977157b
n3ng <n3ng@acs-cseb260-08.ucsd.edu>
2013-11-04 13:03:14 -0800

    Changed the Broadcast Yes/No option to starting your message with "Broadcast" to send to all online users

--- ChatClient/src/main/java/edu/ucsd/cse110/client/ChatClientApplication.java
+++ ChatClient/src/main/java/edu/ucsd/cse110/client/ChatClientApplication.java
@@ -1,6 +1,8 @@
 package edu.ucsd.cse110.client;
 
 import java.net.URISyntaxException;
+import java.util.Scanner;
+
 import javax.jms.JMSException;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
@@ -10,6 +12,7 @@ import javax.jms.Topic;
 import javax.jms.TopicPublisher;
 import javax.jms.TopicSession;
 import javax.jms.TopicSubscriber;
+
 import org.apache.activemq.ActiveMQConnection;
 
 public class ChatClientApplication {
@@ -81,27 +84,27 @@ public class ChatClientApplication {
 	        // TODO GUI: add buttons to display options, such as
 	        // Broadcast: YES, NO
 	        // Make Chat Rooms: YES/NO, Public / Private
-	        System.out.println("Broadcast message? 'Yes' or 'No':");
-//	        Scanner input = new Scanner(System.in);
-	        System.out.println("Yes");
-	        String messageType = "Yes";//input.nextLine();
+	        
+	        // e.g. type "Broadcast hi" to send "hi" to all online users
+	        System.out.println("Begin your message with 'Broadcast' to send it to all online users");
+	        System.out.println("Enter your message:");
+	        Scanner input = new Scanner(System.in);
+	        String inputMessage = input.nextLine();
+	        input.close();
 	        
 	        // TODO if its not a broadcast
-	        if(messageType.equalsIgnoreCase("No")) {
-	        	// TODO set where to send the message	
-//	        	input.close();
-	        }
-	        else if(messageType.equalsIgnoreCase("Yes")) {
+	        if(inputMessage.startsWith("Broadcast") || inputMessage.startsWith("broadcast")) {
 	        	//System.out.println("Enter the message:");
 	        	// TODO input of messages from the key-board
-//	        	client.send("user-name", "Hello World");	// send message
-	        	client.send("Broadcast","Hello World");		// broadcast message
-//				System.out.println("Message Sent!");
-//				input.close();
+	        	// broadcast message
+	        	client.send("Broadcast", inputMessage.substring(inputMessage.indexOf(" ")+1));
+				System.out.println("Message Sent!");
 //		        System.exit(0);
-	        }else {
+
+	        } else {
+	        	// TODO set where to send the message	
+//	        	client.send("user-name", "Hello World");	// send message
 	        	System.out.println("Invalid option, please try again later");
-//	        	input.close();
 //	        	System.exit(-1);
 	        }
 		} catch (JMSException e) {
