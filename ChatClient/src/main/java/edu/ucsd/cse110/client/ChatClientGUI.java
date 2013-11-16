e5ccc8f40800f31f6de0ff023ad59762a35fe149
cs110fbi <cs110fbi@acs-cseb260-23.ucsd.edu>
2013-11-15 18:46:44 -0800

    Added buttons to the main chat room, they do not do anything yet. Added closing behavior to the main frame. (Masha and Bonnie)

--- ChatClient/src/main/java/edu/ucsd/cse110/client/ChatClientGUI.java
+++ ChatClient/src/main/java/edu/ucsd/cse110/client/ChatClientGUI.java
@@ -17,15 +17,12 @@ public class ChatClientGUI extends JFrame {
 		this.setTitle("Welcome to Chat");
 		this.setSize(600, 400);
 		// exit on close
+		this.setCloseOperation();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
-		
-		// adding the main chat panel
 	}
 	
 	/*
-	 * 
+	 * Verifying login
 	 */
 	public void start()  {
 		loginPage = new LoginPageGUI(this, client); // making login page panel
@@ -33,12 +30,15 @@ public class ChatClientGUI extends JFrame {
 		loginPage.log();
 		// continue verifying that login was successful
 		 do {
+			 
 			try {
 				System.out.println("verifying login");
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			System.out.println("verified: " + client.verified);
+			System.out.println("registered: " + client.registered);
 		}while(!(client.verified || client.registered));
 		doChat();
 		
@@ -46,9 +46,30 @@ public class ChatClientGUI extends JFrame {
 	
 	public void doChat()  {
 		
-		this.remove(loginPage);
+		System.out.println("inside doChat");
+		this.remove(loginPage); // clear the frame
+		
 		this.validate();
 		this.repaint();
 		
+        chatPage = new ChatPageGUI(this, client);
+        this.add(chatPage); // adding main chat page to the frame
+        this.validate();
+        this.repaint();
+
+		
+	}
+	/*
+	 * Log out the user when the window is closed.
+	 * Called after user verification.
+	 */
+	private void setCloseOperation() {
+		this.addWindowListener(new java.awt.event.WindowAdapter() {
+		    @Override
+		    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+		        client.sendServer(Constants.LOGOFF, client.getUser());
+		        System.out.println("closing behavior set");
+		    }
+		});
 	}
 }
--- ChatClient/src/main/java/edu/ucsd/cse110/client/ChatPageGUI.java
+++ ChatClient/src/main/java/edu/ucsd/cse110/client/ChatPageGUI.java
@@ -1,5 +1,6 @@
 package edu.ucsd.cse110.client;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -7,14 +8,33 @@ public class ChatPageGUI extends JPanel {
 	
 	private JFrame frame;
 	private ChatClient client;
-	
+	private JButton logoff, createChatRoom, broadcast;
+
 	public ChatPageGUI(JFrame frame, ChatClient client) {
 		
-		frame.removeAll();
-		frame.revalidate();
-		frame.repaint();
 		this.frame = frame;
 		this.client = client;
+		
+		this.frame = frame;
+        this.client = client;
+        this.setSize(400, 400);
+        this.setLayout(null);
+        // configuring the panel
+        logoff = new JButton("Log out");
+        logoff.setLocation(20, 300);
+        logoff.setSize(100, 30);
+        this.add(logoff);
+
+        createChatRoom = new JButton("Create ChatRoom");
+        createChatRoom.setLocation(130, 300);
+        createChatRoom.setSize(160, 30);
+        this.add(createChatRoom);
+
+        broadcast = new JButton("Broadcast");
+        broadcast.setLocation(300, 300);
+        broadcast.setSize(120, 30);
+        this.add(broadcast);
+
 	}
 
 }
